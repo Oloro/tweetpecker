c313409a143f3998fb0cedf19be76d182979f8ba
pawelsikowski <pawel.sikowski@arsokomputery.pl>
2020-06-19 10:32:04 +0200

    add tailwind and lint all

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,26 +4,26 @@ module.exports = {
     node: true
   },
   extends: [
-    "plugin:vue/recommended",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+    'plugin:vue/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
   ],
   parserOptions: {
     ecmaVersion: 2020
   },
   rules: {
-    "prettier/prettier": ["error", {"singleQuote": true}],
-    "quotes": ["error", "single"],
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+    'prettier/prettier': ['error', { singleQuote: true }],
+    quotes: ['error', 'single'],
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
       ],
       env: {
         jest: true

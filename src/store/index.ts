6e2da734733a09fd5d086f56fa94f80a2b917715
pawelsikowski <pawel.sikowski@arsokomputery.pl>
2020-06-22 23:14:07 +0200

    store preperation

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,8 +4,25 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    tweetUrl: '',
+    isTweetLoaded: false
+  },
+  mutations: {
+    SET_TWEET_URL(state, value) {
+      state.tweetUrl = value;
+    },
+    SET_IS_TWEET_LOADED(state, value) {
+      state.isTweetLoaded = value;
+    }
+  },
+  actions: {
+    setTweetUrl(context, url) {
+      context.commit('SET_TWEET_URL', url);
+    },
+    setIsTweetLoaded(context, value) {
+      context.commit('SET_IS_TWEET_LOADED', value);
+    }
+  },
   modules: {}
 });
